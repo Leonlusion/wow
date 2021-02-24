141fa238fa75c317a06fa39346205f24d3e49f6a
Leonlusion <leonstoya@gmail.com>
2021-02-24 20:45:54 +0100

    Create wa-hunter-core.lua

---
+++ wa-hunter-core.lua
@@ -0,0 +1,110 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Aimed Shot 1",
+        [2] = "Aimed Shot 2",
+        [3] = "Focus",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["scale"] = 1,
+    ["xOffset"] = 0,
+    ["config"] = {
+    },
+    ["border"] = false,
+    ["borderEdge"] = "Square Full White",
+    ["regionType"] = "group",
+    ["borderSize"] = 2,
+    ["borderColor"] = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 1,
+    },
+    ["authorOptions"] = {
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["names"] = {
+                },
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["borderInset"] = 1,
+    ["internalVersion"] = 40,
+    ["yOffset"] = 0,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+    },
+    ["id"] = "Core",
+    ["anchorPoint"] = "CENTER",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["selfPoint"] = "CENTER",
+    ["uid"] = "nUqP1uVt(fU",
+    ["borderOffset"] = 4,
+    ["subRegions"] = {
+    },
+    ["conditions"] = {
+    },
+    ["information"] = {
+    },
+    ["load"] = {
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+    },
+}
